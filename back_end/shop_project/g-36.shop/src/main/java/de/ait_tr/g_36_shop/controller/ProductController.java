9163a540460fa26022ce0375cdb08be9b14ea0f9
Leonid Kleimann <k_l_b_1963@hotmail.com>
2024-08-12 21:18:46 +0200

    update

--- back_end/shop_project/g-36.shop/src/main/java/de/ait_tr/g_36_shop/controller/ProductController.java
+++ back_end/shop_project/g-36.shop/src/main/java/de/ait_tr/g_36_shop/controller/ProductController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
+    // DI
     private final ProductService service;
 
     public ProductController(ProductService service) {
--- back_end/shop_project/g-36.shop/src/main/java/de/ait_tr/g_36_shop/service/ProductServiceImpl.java
+++ back_end/shop_project/g-36.shop/src/main/java/de/ait_tr/g_36_shop/service/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package de.ait_tr.g_36_shop.service;
 
 import de.ait_tr.g_36_shop.domain.entity.Product;
+import de.ait_tr.g_36_shop.repository.ProductRepository;
 import de.ait_tr.g_36_shop.service.interfaces.ProductService;
 import org.springframework.stereotype.Service;
 
@@ -10,18 +11,31 @@ import java.util.List;
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    private ProductRepository repository;
+
+    public ProductServiceImpl(ProductRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public Product save(Product product) {
-        return null;
+        return repository.save(product);
     }
 
     @Override
     public List<Product> getAllActiveProducts() {
-        return null;
+        return repository.findAll()
+                .stream()
+                .filter(Product::isActive)
+                .toList();
     }
 
     @Override
     public Product getById(Long id) {
+        Product product = repository.findById(id).orElse(null);
+        if(product != null && product.isActive()){
+            return product;
+        }
         return null;
     }
 
