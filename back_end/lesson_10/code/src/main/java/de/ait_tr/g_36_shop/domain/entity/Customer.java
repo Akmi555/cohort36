140a615618377fc8c487bfd20abd3a8d24b2f743
Leonid Kleimann <k_l_b_1963@hotmail.com>
2024-08-12 22:04:27 +0200

    Lesson update

---
+++ back_end/lesson_10/code/src/main/java/de/ait_tr/g_36_shop/domain/entity/Customer.java
@@ -0,0 +1,8 @@
+package src.main.java.de.ait_tr.g_36_shop.domain.entity;
+
+public class Customer {
+
+    private Long id;
+    private String name;
+    private boolean active;
+}
---
+++ back_end/lesson_10/code/src/main/java/de/ait_tr/g_36_shop/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package src.main.java.de.ait_tr.g_36_shop.repository;
+
+import de.ait_tr.g_36_shop.domain.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
