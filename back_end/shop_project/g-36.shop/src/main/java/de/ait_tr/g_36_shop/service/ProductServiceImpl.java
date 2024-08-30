f8f6cc6bbb7dd11356f89b64359fc2193dce7336
Leonid Kleimann <k_l_b_1963@hotmail.com>
2024-08-30 22:38:08 +0200

    Project update

--- back_end/shop_project/g-36.shop/src/main/java/de/ait_tr/g_36_shop/service/ProductServiceImpl.java
+++ back_end/shop_project/g-36.shop/src/main/java/de/ait_tr/g_36_shop/service/ProductServiceImpl.java
@@ -2,6 +2,9 @@ package de.ait_tr.g_36_shop.service;
 
 import de.ait_tr.g_36_shop.domain.dto.ProductDto;
 import de.ait_tr.g_36_shop.domain.entity.Product;
+import de.ait_tr.g_36_shop.exception_handling.exceptions.NoActiveProductsException;
+import de.ait_tr.g_36_shop.exception_handling.exceptions.ProductNotFoundException;
+import de.ait_tr.g_36_shop.exception_handling.exceptions.SavingProductException;
 import de.ait_tr.g_36_shop.repository.ProductRepository;
 import de.ait_tr.g_36_shop.service.interfaces.ProductService;
 import de.ait_tr.g_36_shop.service.mapping.ProductMappingService;
@@ -24,25 +27,43 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ProductDto save(ProductDto dto) {
         Product entity = mappingService.mapDtoToEntity(dto);
-        repository.save(entity);
+
+        // exception when saving product
+        try {
+            repository.save(entity);
+        } catch (Exception e) {
+            throw new SavingProductException(String.format("Error while saving product: %s", entity), e);
+        }
+
         return mappingService.mapEntityToDto(entity);
     }
 
     @Override
     public List<ProductDto> getAllActiveProducts() {
-        return repository.findAll()
+        List<ProductDto> products = repository.findAll()
                 .stream()
+                .filter(Product::isActive)
                 .map(mappingService::mapEntityToDto)
                 .toList();
+
+        // no active products in the database
+        if (products.isEmpty()) {
+            throw new NoActiveProductsException("There are no active products in the database");
+        }
+
+        return products;
     }
 
     @Override
     public ProductDto getById(Long id) {
         Product product = repository.findById(id).orElse(null);
-        if( product != null && product.isActive()){
+        if (product != null && product.isActive()) {
             return mappingService.mapEntityToDto(product);
         }
-        return null;
+        //return null;
+
+        // product not found exception
+        throw new ProductNotFoundException(id);
     }
 
     @Override
@@ -54,12 +75,12 @@ public class ProductServiceImpl implements ProductService {
     public void deleteById(Long id) {
 
     }
-
+    // product not found exception - ???
     @Override
     public void deleteByTitle(String title) {
 
     }
-
+    // product not found exception - ???
     @Override
     public void restoreById(Long id) {
 
